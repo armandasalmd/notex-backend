ce37af570ee7e2b26e9ac2381ca7e229300785ad
barkausa <barkausa@coventry.ac.uk>
2020-02-28 01:58:34 +0000

    expexted array instead of object fix

--- src/controllers/backpacks.ts
+++ src/controllers/backpacks.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 // TODO: for testing purposes only!
 router.get('/', (req, res) => {
-	Notebook.findOne({ owner: 'test@gmail.com' }).then(notebook => {
+	Notebook.find({ owner: 'test@gmail.com' }).then(notebook => {
 		if (!notebook) {
 			return res.status(404).json({ message: 'Notebook not found' })
 		}
